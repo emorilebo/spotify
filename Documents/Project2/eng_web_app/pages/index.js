158f0341afcaa28f68907918f832f0ed9a6a6d8e
emorilebo <emorylebo@gmail.com>
2021-11-06 08:53:46 +0100

    styling header

--- Documents/Project2/eng_web_app/pages/index.js
+++ Documents/Project2/eng_web_app/pages/index.js
@@ -10,11 +10,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="flex flex-col bg-red-300 h-screen">
+      <div className="flex flex-col  h-screen">
         <Map />
         <div className="flex-1">
-          <div className="">
-            <img src="logo_black.png" alt="" />
+          <div className="flex justify-between items-center">
+            <img className="h-16" src="logo_black.png" alt="" />
+            <div className="flex items-center">
+              <div className="mr-4 w-15 text-sm">Godfrey Lebo</div>
+              <img
+                className="h-12 rounded-full border border-grey-200 p-px"
+                src="profile.jpg"
+                alt=""
+              />
+            </div>
           </div>
         </div>
       </div>
