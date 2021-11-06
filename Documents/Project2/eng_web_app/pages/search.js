c2021aa025221cb7e60649ba1b04398262cd91a6
emorilebo <emorylebo@gmail.com>
2021-11-06 11:18:16 +0100

    search page layout input

--- Documents/Project2/eng_web_app/pages/search.js
+++ Documents/Project2/eng_web_app/pages/search.js
@@ -12,36 +12,53 @@ const Search = () => {
         />
       </div>
       {/* Input container */}
-      <div className="bg-white">
+      <div className="bg-white flex items-center px-4 mb-2">
         {/* FormtoIcons */}
-        <div className="w-10 flex flex-col ">
+        <div className="w-10 flex flex-col mr-2 items-center">
           <img
-            className=""
+            className="h-2.5"
             src="https://img.icons8.com/ios/50/9CA3AF/filled-circle.png"
             alt=""
           />
           <img
-            className=""
+            className="h-10"
             src="https://img.icons8.com/ios/50/9CA3AF/vertical-line.png"
             alt=""
           />
           <img
-            className=""
+            className="h-3"
             src="https://img.icons8.com/windows/50/9CA3AF/filled-circle.png"
             alt=""
           />
         </div>
         {/* input boxes */}
-        <div className="">
+        <div className="flex flex-col flex-1 ">
           {/* Inputs */}
           <input
-            className="h-10 bg-gray-200"
+            className="h-10 bg-gray-200 my-2 rounded-lg p-2 outline-none border-none"
             placeholder="Enter Pickup location"
           ></input>
-          <input className="h-10 bg-gray-200" placeholder="Where to?"></input>
+          <input
+            className="h-10 bg-gray-200 my-2 rounded-2 p-2 outline-none border-none"
+            placeholder="Where to?"
+          ></input>
         </div>
+        {/* Plus Icon */}
+        <img
+          className="w-10 h-10 bg-gray-200 rounded-full ml-3"
+          src="https://img.icons8.com/ios/50/000000/plus-math.png"
+          alt=""
+        />
       </div>
       {/* Saved places */}
+      <div className="flex">
+        <img
+          className="bg-gray-400 w-10 h-10 p-2 rounded-full"
+          src="https://img.icons8.com/ios-filled/50/ffffff/star--v1.png"
+          alt=""
+        />
+        Saved Places
+      </div>
       {/* Confirmed Location */}
     </div>
   );
