824ad8e345113f953ae3ef2c9d7b7d0572f798fd
emorilebo <emorylebo@gmail.com>
2021-11-06 05:32:10 +0100

    project overall Structure

--- Documents/Project2/eng_web_app/pages/index.js
+++ Documents/Project2/eng_web_app/pages/index.js
@@ -2,11 +2,23 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>EngApp</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <div>
+        {/* Wrapper */}
+        <div>
+          {/* Map */}
+          Map
+        </div>
+        <div>
+          {/* Start */}
+          Start
+        </div>
+      </div>
     </div>
   );
 }
