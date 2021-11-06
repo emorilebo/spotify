69fa8ea6032c8127e87687bba6f2d98798a170b5
emorilebo <emorylebo@gmail.com>
2021-11-06 10:58:42 +0100

    search page layout building

--- Documents/Project2/eng_web_app/pages/search.js
+++ Documents/Project2/eng_web_app/pages/search.js
@@ -2,9 +2,42 @@ import React from "react";
 
 const Search = () => {
   return (
-    <div className="">
+    <div className="bg-gray-200 h-screen">
       {/* Button Container */}
+      <div className="bg-white px-4">
+        <img
+          className="h-12"
+          src="https://img.icons8.com/ios-filled/50/000000/left.png"
+          alt=""
+        />
+      </div>
       {/* Input container */}
+      <div className="bg-white">
+        {/* FormtoIcons */}
+        <div className="w-10 flex flex-col ">
+          <img
+            className=""
+            src="https://img.icons8.com/ios/50/9CA3AF/filled-circle.png"
+            alt=""
+          />
+          <img
+            className=""
+            src="https://img.icons8.com/ios/50/9CA3AF/vertical-line.png"
+            alt=""
+          />
+          <img
+            className=""
+            src="https://img.icons8.com/windows/50/9CA3AF/filled-circle.png"
+            alt=""
+          />
+        </div>
+        {/* input boxes */}
+        <div>
+          {/* Inputs */}
+          <div></div>
+          <div></div>
+        </div>
+      </div>
       {/* Saved places */}
       {/* Confirmed Location */}
     </div>
